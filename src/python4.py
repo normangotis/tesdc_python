72cf0044a44e67126436b984f0982918ff180e0f
norman <norman.gotis@gmail.com>
2022-10-07 20:21:14 +0800

    commit python4

---
+++ src/python4.py
@@ -0,0 +1,16 @@
+number1 = 6
+number2 = 3
+sum = number1 + number2
+print(f'The sum of {number1} and {number2} is {sum}')
+
+diff = number1 -  number2
+print(f'The difference of {number1} and {number2} is {diff}')
+
+prod = number1 * number2
+print(f'The product of {number1} and {number2} is {prod}')
+
+quotient = number1 / number2
+print(f'The quotient of {number1} and {number2} is {quotient}')
+
+modulo = number1 % number2
+print(f'The remainder of {number1} and {number2} is {modulo}')
