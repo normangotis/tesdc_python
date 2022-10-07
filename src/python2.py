31f9c098a34a12a5446927c85afcdaf7e78c68e2
norman <norman.gotis@gmail.com>
2022-10-07 15:12:47 +0800

    commit python2

---
+++ src/python2.py
@@ -0,0 +1,13 @@
+number = 10
+
+print("The number is" + str(number))
+print(f'The number is {number}')
+
+name="Norman"
+print(f'The name is {name}')
+
+floatingNumber = 2.0
+print(f'The Floating value is {floatingNumber}')
+
+isValid=True
+print(f'The boolean value is {isValid}')
