0b4f7d8dee91c5acccf5a936ad0c90a02671d670
norman <norman.gotis@gmail.com>
2022-10-08 01:58:59 +0800

    commit pythonactivity2

---
+++ src/pythonactivity2.py
@@ -0,0 +1,6 @@
+a = float(input("Enter Grade in Math: "))
+b = float(input("Enter Grade in English: "))
+c = float(input("Enter Grade in Filipino: "))
+avg = ( a + b + c) / 3
+
+print(f'Average Grade is: {avg}')
